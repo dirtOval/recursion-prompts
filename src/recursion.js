f0e2a1e5433f6d910cf749336f60f758409ccdac
Deirdre <deirdre.loyall@gmail.com>
2022-11-30 15:55:47 -0500

    factorial in recursion.js passing all tests

--- src/recursion.js
+++ src/recursion.js
@@ -6,7 +6,30 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+
+//I: an integer
+//O: an integer
+//C: none
+//E: return null for negative integers
+
+//base case: input is 0
+//return input
+
+//otherwise return input * factorial(n-1)
+
+//if input is zero
+  //return input
+
+//return input * factorial(n-1)
+
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
