5d7bf45107a17a069382a61e7de710437b1b7b5d
Deirdre <deirdre.loyall@gmail.com>
2022-12-01 15:34:10 -0500

    exponent passing all tests

--- src/recursion.js
+++ src/recursion.js
@@ -151,16 +151,20 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
-  var result = base;
+  if (exp > 0) {
+    var result = base;
+  } else if (exp < 0) {
+    var result = 1/base;
+  }
 
   if (exp === 0) {
     return 1;
   }
 
   if (exp > 0) {
-    return base * exponent(base, exp - 1);
+    return result * exponent(base, exp - 1);
   } else if (exp < 0) {
-    return base * exponent(1/base, exp + 1);
+    return result * parseFloat(exponent(base, exp + 1)).toFixed(4);
   }
 };
 
